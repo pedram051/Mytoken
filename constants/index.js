7404ad429cda35154a1b38a7188d1c35216ac3e0
pedram051 <69584152+pedram051@users.noreply.github.com>
2023-08-01 15:24:08 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf31a89efb6f838a0ae7a25273bb3483a5997ae5d";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
